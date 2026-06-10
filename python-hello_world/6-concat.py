6aecb38cee699ae6a7bae47d821ddc87541fbca5
asimhamidwo8 <asimhamidwo8@gmail.com>
2026-06-10 18:16:15 +0300

    Coplete python hello world tasks

---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,3 @@
+str1 = "welcom to "
+str2 = "Holberton School!"
+print(str1 + str2)
---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+str1 = "object_oriented programming with Python"
+str2 = str1[0:6]
+str3 = str1[-6:]
+print(str2 + str3)
